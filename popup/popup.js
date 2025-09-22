d3202d5687bb53fa4cfc52061ac4c10bb9347b0e
meir lerner <meir3252@gmail.com>
2025-09-22 12:52:25 +0300

    markAsRead

--- popup/popup.js
+++ popup/popup.js
@@ -84,7 +84,10 @@ async function displayLatestUpdates() {
                <div class="date-and-actions">
                 <div class="date">${formatDate(item.dateTs)}</div>
                 <div class="actions">
-                 <div class="delete" @click="${() => deleteItem(item.id)}">
+                 <div class="delete" @click="${() => markAsRead(item.id)}" title="Mark as Read">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"><!-- Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE --><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0-18 0"/><path d="m9 12l2 2l4-4"/></g></svg>               
+                 </div>
+                 <div class="delete" @click="${() => deleteItem(item.id)}" title="Delete">
                   <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"><!-- Icon from Tabler Icons by Paweł Kuna - https://github.com/tabler/tabler-icons/blob/master/LICENSE --><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7h16m-10 4v6m4-6v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3"/></svg>
                 </div>
               </div>
@@ -109,6 +112,13 @@ function deleteItem(id) {
   const item = document.querySelector(`.item[data-id="${id}"]`);
   item.remove();
   chrome.runtime.sendMessage({ type: 'deleteItems', ids: [id] });
+}
+
+
+function markAsRead(id) {
+  const item = document.querySelector(`.item[data-id="${id}"]`);
+  item.remove();
+  chrome.runtime.sendMessage({ type: 'markItemsAsRead', ids: [id] });
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
